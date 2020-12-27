9ba18466ae0075456e547850ca0609e3457caa4b
Rees <reesmorriscontact@gmail.com>
2020-12-27 02:36:18 +0000

    Create custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Heading from '../components/heading';
+import Page from '../components/page';
+import Text from '../components/text';
+import Wrapper from '../components/wrapper';
+
+const Index: React.FC = () => {
+  return (
+    <Page>
+      <Wrapper>
+        <Heading>404 - Not Found</Heading>
+        <Text>Sorry, but this page doesn&apos;t exist!</Text>
+      </Wrapper>
+    </Page>
+  );
+};
+
+export default Index;
